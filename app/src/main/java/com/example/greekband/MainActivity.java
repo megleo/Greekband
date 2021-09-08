782960e193ef7c8b12b1c2216d69143fab8af2de
ml3082298291 <3082298291@qq.com>
2021-09-08 16:49:26 +0800

    GreekBand01

--- app/src/main/java/com/example/greekband/MainActivity.java
+++ app/src/main/java/com/example/greekband/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.greekband;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int mClickCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mClickCount++;
+                Toast.makeText(MainActivity.this,"啊，我被击中了" + mClickCount + "次",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
